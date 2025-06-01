5fd6e901fff43e5c810b4f32fcd0353d92b93ee5
Daniel Mendler <mail@daniel-mendler.de>
2025-06-01 09:42:12 +0200

    Move gnuplot--setup-menubar below variable definitions

--- gnuplot.el
+++ gnuplot.el
@@ -594,38 +594,6 @@ create a `gnuplot-mode' buffer."
 This part contains the toggle buttons for displaying info or
 opening an argument-setting popup.")
 
-(defun gnuplot--setup-menubar ()
-  "Initial setup of gnuplot and insertions menus."
-  (unless gnuplot-mode-menu
-    (when gnuplot-insertions-menu-flag
-      (let ((insertions-menu
-             `("Insertions"
-               ,@gnuplot-insertions-top
-               ,gnuplot-insertions-adornments
-               ,gnuplot-insertions-plot-options
-               ,gnuplot-insertions-terminal
-               ,gnuplot-insertions-x-axis
-               ,gnuplot-insertions-y-axis
-               ,gnuplot-insertions-z-axis
-               ,gnuplot-insertions-x2-axis
-               ,gnuplot-insertions-y2-axis
-               ,gnuplot-insertions-parametric-plots
-               ,gnuplot-insertions-polar-plots
-               ,gnuplot-insertions-surface-plots
-               ,@gnuplot-insertions-bottom)))
-        (easy-menu-define gnuplot--insertions-menu gnuplot-mode-map
-          "Insertions menu used in Gnuplot-mode"
-          insertions-menu)))
-    (easy-menu-define                     ; set up gnuplot menu
-      gnuplot-mode-menu gnuplot-mode-map "Menu used in gnuplot-mode"
-      gnuplot--menu)))
-
-(defun gnuplot--mark-active ()
-  "Return non-nil if the mark is active and it is not equal to point."
-  (condition-case nil
-      (and (mark) (/= (mark) (point)))
-    (error nil)))
-
 (defvar gnuplot-recently-sent nil
   "This is a record of the most recent kind of text sent to gnuplot.
 It takes as its value nil, `line', `region', `buffer', or `file'.  It is
@@ -1873,6 +1841,38 @@ a list:
             #'gnuplot--syntax-propertize-extend-region nil t)
   (setq gnuplot--comint-recent-buffer (current-buffer))
   (gnuplot--setup-menubar))
+
+(defun gnuplot--setup-menubar ()
+  "Initial setup of gnuplot and insertions menus."
+  (unless gnuplot-mode-menu
+    (when gnuplot-insertions-menu-flag
+      (let ((insertions-menu
+             `("Insertions"
+               ,@gnuplot-insertions-top
+               ,gnuplot-insertions-adornments
+               ,gnuplot-insertions-plot-options
+               ,gnuplot-insertions-terminal
+               ,gnuplot-insertions-x-axis
+               ,gnuplot-insertions-y-axis
+               ,gnuplot-insertions-z-axis
+               ,gnuplot-insertions-x2-axis
+               ,gnuplot-insertions-y2-axis
+               ,gnuplot-insertions-parametric-plots
+               ,gnuplot-insertions-polar-plots
+               ,gnuplot-insertions-surface-plots
+               ,@gnuplot-insertions-bottom)))
+        (easy-menu-define gnuplot--insertions-menu gnuplot-mode-map
+          "Insertions menu used in Gnuplot-mode"
+          insertions-menu)))
+    (easy-menu-define                     ; set up gnuplot menu
+      gnuplot-mode-menu gnuplot-mode-map "Menu used in gnuplot-mode"
+      gnuplot--menu)))
+
+(defun gnuplot--mark-active ()
+  "Return non-nil if the mark is active and it is not equal to point."
+  (condition-case nil
+      (and (mark) (/= (mark) (point)))
+    (error nil)))
 
 ;;;###autoload
 (defun gnuplot-make-buffer ()
